f38fa78df02942bfb7275d355e727bb5f0bde5bd
Mitesh <miteshprajapati6439@gmail.com>
2025-03-18 21:24:41 +0530

    try to done TO-DO list

---
+++ To-Do/to-do.js
@@ -0,0 +1,44 @@
+const todoList =[{name : 'mitesh', dueDate:'04-03-2025'}];
+
+renderList();
+
+function renderList() {
+  let todoHTML = '';
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i]
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject;
+
+    const html = `
+    <div>
+    ${name}</div><div>${dueDate}</div>
+    
+    <button onclick="todoList.splice(${i},1)
+    renderList();">
+    Delete
+    </button>
+    `;
+    todoHTML+=html;
+  }
+  document.querySelector('.list').innerHTML=todoHTML;
+}
+function addName() {
+  const inputName =  document.querySelector('.js-input-name');
+  const inputDate = document.querySelector('.js-date');
+  const name = inputName.value;
+  const dueDate = inputDate.value;
+
+  todoList.push(
+    {
+      // name : name,
+      // dueDate : date
+      name,
+      dueDate
+    }
+  );
+  console.log(todoList);
+
+  inputName.value = '';
+  renderList();
+}
