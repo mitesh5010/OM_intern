4eb69ed782ddb63635c52e0acf3a73820605c8d8
Mitesh <miteshprajapati6439@gmail.com>
2025-03-21 11:09:09 +0530

    load the cart in local storage  to modify the cart file

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+ cart =[
 
   {
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -8,6 +10,12 @@ export let cart = [
     quantity: 1
   }
 ];
+}
+
+// update cart and store in local storage
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,6 +33,7 @@ export function addToCart(productId) {
           quantity: 1
         });
       }
+      saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +46,6 @@ export function removeFromCart(productId) {
   });
   
   cart =newCart;
+
+  saveToStorage();
 }
