c242eed373b64c2b386a4d9faffda31ca6768881
Mitesh <miteshprajapati6439@gmail.com>
2025-03-25 16:19:04 +0530

    learn Fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,15 +2,12 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymetSummary } from "./checkout/paymentSummary.js";
 // import   '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+
 Promise.all([
-  new Promise((resolve)=>{
-    loadProducts(()=>{
-      resolve();
-    })
-  }),
+  loadProductsFetch(),
   new Promise((resolve)=>{
     loadCart(()=>{
       resolve();
